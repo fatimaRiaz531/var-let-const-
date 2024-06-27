b9bbfd31f7cf860d706987b0a12b612b19826128
fatima <162475613+fatimaRiaz531@users.noreply.github.com>
2024-06-27 15:34:35 +0500

    var/let/const

---
+++ letconst.js
@@ -0,0 +1,15 @@
+console.log("javascript : let, const");
+//var a =45;
+//var a = "z"
+var b = "Fatima";
+var author = "Fatima";
+author = 5; //throws an error because constant cannot be changed
+b = 4;
+var harry = 0;
+var c = null;
+var d = undefined;
+{
+    var b_1 = "this";
+    console.log(b_1);
+}
+console.log(b);
---
+++ letconst.ts
@@ -0,0 +1,16 @@
+console.log("javascript : let, const")
+//var a =45;
+//var a = "z"
+let b  = "Fatima";
+const author = "Fatima";
+author = 5  //throws an error because constant cannot be changed
+b= 4
+const harry =0;
+let  c = null
+let d = undefined
+{
+    let b = "this"
+    console.log(b)
+}
+console.log(b)
+
